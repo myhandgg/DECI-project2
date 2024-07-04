011ce81a0d07419bc07bbbb01dd5d4aefb745b91
Eslam Tharwat <eslamproo@yahoo.com>
2024-07-03 21:31:30 -0700

    fixing some issues

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# DECI-project2
-DECI second project
+# DECI project-2
+
+## Description
+DECI project-2 is a project which you can go through sections and add comments dynamically
+
+## Installation
+1. Clone the repository `git clone https://github.com/myhandgg/DECI-project2.git`
+2. Navigate to the project directory `cd DECI-project2`
+3. Install dependencies: `npm install`
+4. Start the application: `npm start`
+
+## Usage
+the functionality is to create a comment
+
+1. the first loop is looping over all the sections and put it into unordered list
+
+2. on scroll we compare the y axis to know which section is currently active
+ = and the removeActive function removes every active class and put it into the current one
+
+3. the createComment function makes a comment with the name, email and description of the user
+4. on submit we check if any of the inputs are invalid and if so, we show an error message
+
+## Contributing
+Contributions are welcome! Please fork the repository and submit a pull request with your changes.
+
+## License
+This project is licensed under the MIT License.
--- js/script.js
+++ js/script.js
@@ -1,94 +1,109 @@
-const ul = document.querySelector("ul")
-const sections = document.querySelectorAll("section")
+const ul = document.querySelector("ul");
+const sections = document.querySelectorAll("section");
 
-let num = 1
+let num = 1;
 
+// looping over sections and put the title in the nav bar
 for (let i = 0; i < sections.length; i++) {
-    const list = document.createElement("li")
-    const link = document.createElement("a")
-    
-    link.href = `#${sections[i].children[0].children[0].textContent.split(" ")[0].toLocaleLowerCase()}-${num}`
-    link.textContent = sections[i].children[0].children[0].textContent
-    
-    list.appendChild(link)
-    
-    ul.appendChild(list)
-    num++
-}
- 
-window.onscroll = () => {
-    if(window.scrollY >= 0 && window.scrollY < 800) {
-        removeActive()
-        sections[0].classList.add("active")
-    } else if (window.scrollY >= 769 && window.scrollY < 1472) {
-        removeActive()
-        sections[1].classList.add("active")
-    } else if (window.scrollY >= 1800 && window.scrollY < 2400) {
-        removeActive()
-        sections[2].classList.add('active')
-    } else if (window.scrollY >= 2600 && window.scrollY < 3340){
-        removeActive()
-        sections[3].classList.add("active")
-    } else if (window.scrollY >= 3540 && window.scrollY < 4330){
-        removeActive()
-        sections[4].classList.add("active")
-    }
-     else {
-        for (let i = 0; i < sections.length; i++) {
-            sections[i].classList.remove("active")
-        }
-    }
+  const list = document.createElement("li");
+  const link = document.createElement("a");
+
+  link.href = `#${sections[i].children[0].children[0].textContent
+    .split(" ")[0]
+    .toLocaleLowerCase()}-${num}`;
+  link.textContent = sections[i].children[0].children[0].textContent;
+
+  list.appendChild(link);
+
+  ul.appendChild(list);
+  num++;
 }
 
+// handling active section
+window.onscroll = () => {
+  if (window.scrollY >= 0 && window.scrollY < 800) {
+    removeActive();
+    sections[0].classList.add("active");
+  } else if (window.scrollY >= 769 && window.scrollY < 1472) {
+    removeActive();
+    sections[1].classList.add("active");
+  } else if (window.scrollY >= 1800 && window.scrollY < 2400) {
+    removeActive();
+    sections[2].classList.add("active");
+  } else if (window.scrollY >= 2600 && window.scrollY < 3340) {
+    removeActive();
+    sections[3].classList.add("active");
+  } else if (window.scrollY >= 3540 && window.scrollY < 4330) {
+    removeActive();
+    sections[4].classList.add("active");
+  } else {
+    for (let i = 0; i < sections.length; i++) {
+      sections[i].classList.remove("active");
+    }
+  }
+};
+
+// removing active class from others and add it to the current one
 function removeActive() {
-    sections.forEach(e => {
-        e.classList.remove("active")
-    })
+  sections.forEach((e) => {
+    e.classList.remove("active");
+  });
 }
 
 // Comment functionality
 
-const nameInput = document.querySelector("#name")
-const emailInput = document.querySelector("#email")
-const commentInput = document.querySelector("#comment")
-const commentDiv = document.querySelector("#comments-6 .user-comment")
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const commentInput = document.querySelector("#comment");
+const commentDiv = document.querySelector("#comments-6 .user-comment");
 
-const btn = document.querySelector(".submit-btn")
+const form = document.querySelector("form");
 
-btn.addEventListener("click" , function() {
-    if (!nameInput.value || !emailInput.value || !commentInput.value) return
-    createComment()
-})
+const nameInputError = document.querySelector(".name-error");
+const emailInputError = document.querySelector(".email-error");
+const commentInputError = document.querySelector(".comment-error");
 
+// handling submitting the comment
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  // handling if one of the inputs not valid
+  if (!nameInput.value.trim()) {
+    nameInputError.classList.remove("hide");
+  } else {
+    nameInputError.classList.add("hide");
+  }
+  if (!emailInput.value.trim()) {
+    emailInputError.classList.remove("hide");
+  } else {
+    emailInputError.classList.add("hide");
+  }
+  if (!commentInput.value.trim()) {
+    commentInputError.classList.remove("hide");
+    return;
+  } else {
+    commentInputError.classList.add("hide");
+  }
+  // handling when all the inputs are valid
+  if (nameInput.value !== "" && emailInput.value !== "" && commentInput.value !== "") {
+    createComment();
+  }
+});
+
+// function that creates user comment
 function createComment() {
-    const commentContainer = document.createElement("div")
-    commentContainer.classList.add("comment")
-    
-    const comment = document.createElement("p")
-    
-    const em = document.createElement("em")
-    em.textContent = commentInput.value
-    
-    comment.appendChild(em)
+  const commentContainer = document.createElement("div");
+  commentContainer.classList.add("comment");
 
-    const emailContainer = document.createElement("div")
-    emailContainer.classList.add("email")
+  commentContainer.innerHTML = `
+  <p><em>${commentInput.value}</em></p>
+  <div class="email">
+    <h4>${nameInput.value}</h4>
+    <p>${emailInput.value}</p>
+  </div>`
 
-    const userName = document.createElement("h4")
-    userName.textContent = nameInput.value
+  commentDiv.prepend(commentContainer);
 
-    const userEmail = document.createElement("p")
-    userEmail.textContent = emailInput.value
-
-    emailContainer.appendChild(userName)
-    emailContainer.appendChild(userEmail)
-
-    commentContainer.appendChild(comment)
-    commentContainer.appendChild(emailContainer)    
-
-    commentDiv.prepend(commentContainer)
-
-    nameInput.value = ""
-    emailInput.value = ""
-    commentInput.value = ""
+  nameInput.value = "";
+  emailInput.value = "";
+  commentInput.value = "";
 }
